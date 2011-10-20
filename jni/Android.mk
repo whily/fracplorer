d31f0644285d0999c60fe64c7d9c3cf6902d6c76
Yujian Zhang <yujian.zhang@gmail.com>
2011-10-20 21:54:47 +0800

    Add Android.mk.

---
+++ jni/Android.mk
@@ -0,0 +1,19 @@
+# NDK makefile.
+#
+# Author:
+#   Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+#
+# License: 
+#   GNU General Public License v2
+#   http://www.gnu.org/licenses/gpl-2.0.html
+# Copyright (C) 2011 Yujian Zhang
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := mandelbrot
+LOCAL_SRC_FILES := mandelbrot.c
+LOCAL_CFLAGS    += -Wall
+
+include $(BUILD_SHARED_LIBRARY)
