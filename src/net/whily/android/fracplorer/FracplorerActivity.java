768e4309a4ab04288f30b7f4da94a5ece5a309c1
Yujian Zhang <yujian.zhang@gmail.com>
2014-01-12 17:39:39 +0800

    Use immersive mode for FracplorerActivity.

--- src/net/whily/android/fracplorer/FracplorerActivity.java
+++ src/net/whily/android/fracplorer/FracplorerActivity.java
@@ -14,18 +14,21 @@ package net.whily.android.fracplorer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
 
 public class FracplorerActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     
-    // Full screen mode.
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                         WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    // Enter immersive mode.
+    getWindow().getDecorView().
+      setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | 
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE);
     
     setContentView(R.layout.main);
   }
